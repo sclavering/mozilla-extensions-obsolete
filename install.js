41db3f163859198bdfcd4c88766a074405160e27
Stephen Clavering <stephen@clavering.me.uk>
2003-09-05 15:07:35 +0000

    increasing version number to 0.6
    updating desc to reflect removal of StyleSelector and change of menuitem name
    
    simplify installation script (including removal of success message)

--- install.js
+++ install.js
@@ -1,39 +1,31 @@
 const APP_DISPLAY_NAME = "Link Toolbar";
 const APP_NAME = "linktoolbar";
 const APP_PACKAGE = "/cdn.mozdev.org/linktoolbar";
-const APP_VERSION = "0.2";
+const APP_VERSION = "0.6";
 
 const APP_JAR_FILE = "linktoolbar.jar";
 const APP_CONTENT_FOLDER = "content/linktoolbar/";
 const APP_LOCALE_FOLDER  = "locale/en-US/linktoolbar/";
 const APP_SKIN_FOLDER  = "skin/classic/linktoolbar/";
 
-const APP_SUCCESS_MESSAGE = "You may need to restart your browser first.";
-
 const INST_TO_PROFILE = "Do you wish to install "+APP_DISPLAY_NAME+" to your profile?\nThis will mean it does not need reinstalling when you update your browser.\n(Click Cancel if you want "+APP_DISPLAY_NAME+" installing to the main browser directory.)";
 
 initInstall(APP_NAME, APP_PACKAGE, APP_VERSION);
 
-// profile installs only work since 2003-03-06
-var instToProfile = (buildID>2003030600 && confirm(INST_TO_PROFILE));
+var instToProfile = confirm(INST_TO_PROFILE);
 
 var chromef = instToProfile ? getFolder("Profile", "chrome") : getFolder("chrome");
+var flag = instToProfile ? PROFILE_CHROME : DELAYED_CHROME;
+
 var err = addFile(APP_PACKAGE, APP_VERSION, APP_JAR_FILE, chromef, null)
 if(err == SUCCESS) {
 	var jar = getFolder(chromef, APP_JAR_FILE);
-  if(instToProfile) {
-  	registerChrome(CONTENT | PROFILE_CHROME, jar, APP_CONTENT_FOLDER);
-  	registerChrome(LOCALE  | PROFILE_CHROME, jar, APP_LOCALE_FOLDER);
-  	registerChrome(SKIN  | PROFILE_CHROME, jar, APP_SKIN_FOLDER);
-  } else {
-  	registerChrome(CONTENT | DELAYED_CHROME, jar, APP_CONTENT_FOLDER);
-  	registerChrome(LOCALE  | DELAYED_CHROME, jar, APP_LOCALE_FOLDER);
-  	registerChrome(SKIN  | DELAYED_CHROME, jar, APP_SKIN_FOLDER);
-  }
+	registerChrome(CONTENT | flag, jar, APP_CONTENT_FOLDER);
+	registerChrome(LOCALE  | flag, jar, APP_LOCALE_FOLDER);
+	registerChrome(SKIN    | flag, jar, APP_SKIN_FOLDER);
+
 	err = performInstall();
-	if(err == SUCCESS || err == 999) {
-		alert(APP_DISPLAY_NAME+" "+APP_VERSION+" has been succesfully installed.\n"+APP_SUCCESS_MESSAGE);
-	} else {
+	if(err != SUCCESS && err != 999) {
 		alert("Install failed. Error code:" + err);
 		cancelInstall(err);
 	}
