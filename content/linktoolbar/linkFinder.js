621247a25181e3d522f1c7e7af197e9996b48a4b
Stephen Clavering <stephen@clavering.me.uk>
2004-01-17 17:51:56 +0000

    *** empty log message ***

--- content/linktoolbar/linkFinder.js
+++ content/linktoolbar/linkFinder.js
@@ -3,7 +3,7 @@ const linkFinder = {
   // XXX some pages use << for first and < for prev, so we should handle things like that differently
   re_first: /^first|first$|^begin|\|<|\u00ab/i, // ? >\u007c| ?
   re_prev:  /^prev(ious)?\b|prev$|previous$|^back\b|\bback$|^<<?-?$|\u00ab/i, // \u003c / = | <=
-  re_next:  /^next\b|^more\b|continue\b|next$|^[-]?>[>]?$|\u00bb/i,
+  re_next:  /^next\b|^more\b|continue\b|next$|^-?>?>$|\u00bb/i,
   re_last:  /^last|last$|^end\b|>\|/i, // ? >\u007c| ?
 
   // regular expressions used for identifying links based on the src url of contained images
