dde6ddf0c7d17dad119e54b9356061c8a23902dd
Stephen Clavering <stephen@clavering.me.uk>
2004-01-17 16:49:05 +0000

    adding detection of fst/prv/nxt/last links based on code from LinkIt
    should handle rel attributes on <a href="..."/> links too, though that's not been tested.

--- content/linktoolbar/linkFinder.js
+++ content/linktoolbar/linkFinder.js
@@ -1,33 +1,103 @@
 const linkFinder = {
+  // regular expressions for identifying link types
+  // XXX some pages use << for first and < for prev, so we should handle things like that differently
+  re_first: /^first|first$|^begin|\|<|\u00ab/i, // ? >\u007c| ?
+  re_prev:  /^prev(ious)?\b|prev$|previous$|^back\b|\bback$|^<<?-?$|\u00ab/i, // \u003c / = | <=
+  re_next:  /^next\b|^more\b|continue\b|next$|^[-]?>[>]?$|\u00bb/i,
+  re_last:  /^last|last$|^end\b|>\|/i, // ? >\u007c| ?
+
+  // regular expressions used for identifying links based on the src url of contained images
+  re_first2: /first/i,
+  re_prev2:  /p?rev/i,
+  re_next2:  /next|more|fwd/i,
+  re_last2:  /last/i,
+
   findLinks: function(doc) {
+    var i, j, link;
+
     if(!("__lt__links" in doc)) doc.__lt__links = new Array();
 
-    // add 'up' and 'top' links if we don't already have them
-    var noTop = true, noUp = true;
-    for(var i = 0; (noTop || noUp) && i < doc.__lt__links.length; i++) {
-      var link = doc.__lt__links[i];
+    // work out which reltypes to generate links for
+    var noTop = true, noUp = true, noPrev = true, noNext = true;
+    for(i = 0; (noTop || noUp || noPrev || noNext) && i < doc.__lt__links.length; i++) {
+      link = doc.__lt__links[i];
       if("top" in link.relValues) noTop = false;
       if("up" in link.relValues) noUp = false;
+      if("prev" in link.relValues) noPrev = false;
+      if("next" in link.relValues) noNext = false;
     }
+
+    // generate top and up links based on url
     if(noTop) {
       var topurl = doc.location.href.match(/^[^\/]*?:\/\/[^\/]*\//);
-      if(topurl) {
-        topurl = topurl[0];
-        var toprels = [];
-        toprels["top"] = "top";
-        var top = {href: topurl, relValues: toprels, title: null, longTitle: null};
-        linkToolbarUI.addLink(top, doc);
-      }
+      if(topurl) this.addLink(doc, topurl[0], null, "top", null, null);
     }
     if(noUp) {
       var upurl = this.getUp(doc.location.href);
-      if(upurl) {
-        var uprels = [];
-        uprels["up"] = "up";
-        var up = {href: upurl, relValues: uprels, title: null, longTitle: null};
-        linkToolbarUI.addLink(up, doc);
-      }
+      if(upurl) this.addLink(doc, upurl, null, "up", null, null);
     }
+
+    // generate other links based on <a href="..."/> style links
+
+    if(!(noPrev || noNext)) return;
+
+
+    // xxx use NS version of function?
+    var links = doc.getElementsByTagName('a');
+
+    for(i = 0; i < links.length; i++) {
+      link = links[i];
+      var href = link.getAttribute("href");
+      var rel = link.getAttribute("rel");
+      var base = link.baseURI; // DOM3, needed because we have to resolve relative urls
+
+      // ignore non link <a>s, and internal links
+      if(!href || href.charAt(0)=='#') continue;
+
+      var title = this.getText(link).replace(/\s+/g," ");
+
+      // Do The Right Thing for <a href="..." rel="..."/>  :)
+      if(rel) {
+        // get rel types
+        var rawRels = rel.split(/\s+/);
+        var rels = [];
+        for(j = 0; j < rawRels.length; j++) {
+          var aRel = linkToolbarHandler.standardiseRelType(rawRels[j]);
+          // avoid duplicate rel values
+          if(rel) rels[aRel] = aRel;
+        }
+        if(rels.length==0) continue;
+        // add the link
+        var info = {href: href, relValues: rels, title: title, longTitle: null};
+        linkToolbarUI.addLink(info, doc);
+        // don't bother with hackery below
+        continue;
+      }
+
+      if(this.re_next.test(title))
+        this.addLink(doc, href, base, "next", title, null);
+      else if(this.re_prev.test(title))
+        this.addLink(doc, href, base, "prev", title, null);
+      else if(this.re_first.test(title))
+        this.addLink(doc, href, base, "first", title, null);
+      else if(this.re_last.test(title))
+        this.addLink(doc, href, base, "last", title, null);
+    }
+  },
+
+  addLink: function(doc, url, base, rel, title, longTitle) {
+  	// resolve relative urls
+  	if(base) {
+    	var baseuri = Components.classes["@mozilla.org/network/standard-url;1"]//.createInstance();
+    	                        .createInstance(Components.interfaces.nsIURI);
+    	baseuri.spec = base;
+    	url = baseuri.resolve(url);
+    }
+    // add the link
+    var rels = [];
+    rels[rel] = rel;
+    var info = {href: url, relValues: rels, title: title, longTitle: longTitle};
+    linkToolbarUI.addLink(info, doc);
   },
 
   // borrowed from GoUp
@@ -52,5 +122,31 @@ const linkFinder = {
     if(matches) return (protocol+matches[1]);
     // nothing found
     return null;
+  },
+
+  // get the text contained in a link
+  // XXX rewrite this to be less horrible
+  getText: function(el) {
+    // use alt text for images
+    if(el.nodeName.toLowerCase() == "img") {
+      // should this have spaces wrapped round it?
+      s = el.getAttribute("alt");
+      // hacky test of image src url because authors don't use alt :(
+      var src = el.getAttribute("src");
+      if(this.re_next2.test(src)) s += " next";
+      else if(this.re_prev2.test(src)) s += " prev";
+      else if(this.re_first2.test(src)) s += " first";
+      else if(this.re_last2.test(src)) s += " last";
+      return s;
+    }
+    // deal with other elements
+    var s = "", kids = el.childNodes;
+    for(var i = 0; i < kids.length; i++) {
+      // add contents of CDATA or text nodes
+      if(kids[i].nodeType==3 || kids[i].nodeType==2) s += kids[i].nodeValue;
+      // call recursively for elements
+      else if(kids[i].nodeType==1) s += this.getText(kids[i]);
+    }
+    return s;
   }
 }
