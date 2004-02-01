c30c17893ab8e1fcc235450f0f8ea1bd499738f9
Stephen Clavering <stephen@clavering.me.uk>
2004-02-01 17:54:25 +0000

    0.12

--- install.js
+++ install.js
@@ -1,7 +1,7 @@
 const kDisplayName = "Toolbar Enhancements";
 const kName = "toolbarext";
 const kPackage = "/clav.mozdev.org/toolbarext";
-const kVersion = "0.11";
+const kVersion = "0.12";
 
 const kJarFile = "toolbarext.jar";
 const kContentFolder = "content/";
