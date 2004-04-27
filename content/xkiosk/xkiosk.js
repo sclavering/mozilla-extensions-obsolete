4cb09a437dc3e54b9b53e2dd3f4de45cf392876b
Stephen Clavering <stephen@clavering.me.uk>
2004-04-27 13:04:59 +0000

    make clearing history work in builds from Feb. onward

--- content/xkiosk/xkiosk.js
+++ content/xkiosk/xkiosk.js
@@ -58,17 +58,19 @@ function clearAll() {
 }
 
 function clearHistory() {
-  var globalHistory = Components.classes["@mozilla.org/browser/global-history;1"]
-                                .getService(Components.interfaces.nsIBrowserHistory);
-  globalHistory.removeAllPages();
-  return true;
+  // builds from 20040210ish onward use the second class.  the relevant parts
+  // of the nsIBrowserHistory are not altered though.
+  var class1 = "@mozilla.org/browser/global-history;1";
+  var class2 = "@mozilla.org/browser/global-history;2";
+  var history = (class2 in Components.classes) ? class2 : class1;
+  history = Components.classes[history].getService(Components.interfaces.nsIBrowserHistory);
+  history.removeAllPages();
 }
 
 function clearFormInfo() {
   var formHistory = Components.classes["@mozilla.org/satchel/form-history;1"]
                               .getService(Components.interfaces.nsIFormHistory);
   formHistory.removeAllEntries();
-  return true;
 }
 
 function clearPasswords() {
@@ -84,8 +86,6 @@ function clearPasswords() {
 
   for (var i = 0; i < passwds.length; ++i)
     passwdMgr.removeUser(passwds[i].host, passwds[i].user);
-
-  return true;
 }
 
 function clearDownloads() {
@@ -93,7 +93,7 @@ function clearDownloads() {
   try {
     var downloads = getDownloads();
   } catch (e) {
-    return true;
+    return;
   }
 
   var rdfs = Components.classes["@mozilla.org/rdf/rdf-service;1"].getService(Components.interfaces.nsIRDFService);
@@ -116,8 +116,6 @@ function clearDownloads() {
   var rds = ds.QueryInterface(Components.interfaces.nsIRDFRemoteDataSource);
   if (rds)
     rds.Flush();
-
-  return true;
 }
 
 function getDownloads() {
@@ -147,8 +145,6 @@ function clearCookies() {
 
   for (var i = 0; i < cookies.length; ++i)
     cookieMgr.remove(cookies[i].host, cookies[i].name, cookies[i].path, false);
-
-  return true;
 }
 
 function clearCache() {
@@ -160,6 +156,4 @@ function clearCache() {
 
   clearCacheOfType(Components.interfaces.nsICache.STORE_ON_DISK);
   clearCacheOfType(Components.interfaces.nsICache.STORE_IN_MEMORY);
-
-  return true;
 }
