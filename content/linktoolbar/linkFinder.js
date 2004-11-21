d28d7d76aa656e9e53797794fcc8c37b635b322c
Stephen Clavering <stephen@clavering.me.uk>
2004-11-21 18:48:50 +0000

    remove the double-right-angle-quote symbol from the regexp for Next links because it leads to many incorrect matches (incl. Google search results pages and cd-wow.com)

--- content/linktoolbar/linkFinder.js
+++ content/linktoolbar/linkFinder.js
@@ -2,8 +2,8 @@ var linkToolbarLinkFinder = {
   // regular expressions for identifying link types
   // XXX some pages use << for first and < for prev, so we should handle things like that differently
   re_first: /^first\b|\bfirst$|^begin|\|<|\u00ab/i, // ? >\u007c| ?
-  re_prev:  /^prev(ious)?\b|prev$|previous$|^back\b|\bback$|^<<?-?$|\u00ab/i, // \u003c / = | <=
-  re_next:  /^next\b|\bcontinue\b|next$|^-?>?>$|\u00bb$/i,
+  re_prev:  /^prev(?:ious)?\b|prev$|previous$|^back\b|\bback$|^<<?-?$|\u00ab/i, // \u003c / = | <=
+  re_next:  /^next\b|\bcontinue\b|next$|^-?>?>$/i, // |\u00bb$/i,
   re_last:  /^last\b|\blast$|^end\b|>\|/i, // ? >\u007c| ?
 
   // regular expressions used for identifying links based on the src url of contained images
