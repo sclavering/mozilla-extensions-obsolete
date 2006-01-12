bd47997e64a0b3e01c0405cb1633af3fee29d024
Stephen Clavering <stephen@clavering.me.uk>
2006-01-12 23:53:46 +0000

    remove obsolete pref

--- defaults/preferences/linkwidget.js
+++ defaults/preferences/linkwidget.js
@@ -1,4 +1,3 @@
-pref("extensions.linkwidget.iconsOnly", true);
 pref("extensions.linkwidget.guessUpAndTopFromURL", true);
 pref("extensions.linkwidget.guessPrevAndNextFromURL", false);
 pref("extensions.linkwidget.scanHyperlinks", true);
